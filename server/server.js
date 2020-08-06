258ff8591de3d8161895018b12c34feae9b2d766
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-08-06 13:19:16 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 //packages required
 const express = require('express');
+var path = require('path');
 const http = require('http');
 
 //hostname and port
 const hostname = 'localhost';
 const port = 3000;
+
+//express set up
+const app = express();
+
+//
+// app.get('/',function(req,res){
+//     res.sendFile(__dirname + '/static'+'/classify.html');
+// });
+
+// app.use(express.static(path.join(__dirname+'/classify.html')));
+
+//server setup with http
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
